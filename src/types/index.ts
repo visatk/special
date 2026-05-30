162c893c00d926d08ec05d3d63c47b3c92a335dd
visatk <visatk29@gmail.com>
2026-05-30 21:56:01 +0600

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+import { Context } from 'grammy';
+
+export interface Env {
+	DB: D1Database;
+	TELEGRAM_BOT_TOKEN: string;
+	WEBHOOK_SECRET: string;
+}
+
+export type BotContext = Context & {
+	env: Env;
+};
