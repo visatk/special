779ad5d916f89a9f0a09dcaf8fc9c9392125fe4b
visatk <visatk29@gmail.com>
2026-05-30 22:23:53 +0600

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ export interface Env {
 	DB: D1Database;
 	TELEGRAM_BOT_TOKEN: string;
 	WEBHOOK_SECRET: string;
+	ADMIN_CHAT_ID: string; // The group or admin ID where messages are sent
 }
 
 export type BotContext = Context & {
